9924d6f19b3c4314322b8e5887c65160c1432ac2
arun-0610 <e.arunkumar610@gmail.com>
2025-12-06 18:00:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   // If your frontend sends cookies or Authorization with credentials:
-  // res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Credentials', 'true');
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
   }
